798ee1bf564012ba9d0be5f754375ddfc1fd45be
yves <yves@Harlekin-PC>
2018-11-04 14:42:09 +0100

    Next try

--- src/test/java/ch/yves/micronaut/controller/TestDocker.java
+++ src/test/java/ch/yves/micronaut/controller/TestDocker.java
@@ -5,7 +5,9 @@ import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.shaded.org.apache.http.HttpEntity;
 import org.testcontainers.shaded.org.apache.http.client.methods.CloseableHttpResponse;
 import org.testcontainers.shaded.org.apache.http.client.methods.HttpGet;
@@ -16,15 +18,15 @@ import org.testcontainers.shaded.org.apache.http.util.EntityUtils;
 public class TestDocker {
 
 
-    //@Rule
-    //public GenericContainer dslContainer = new GenericContainer("gandalf1973/micronaut:latest");
+    @Rule
+    public GenericContainer dslContainer = new GenericContainer("gandalf1973/micronaut:latest");
     	
 
     @Test
     public void simpleDslTest() throws IOException {
-        //String address = String.format("http://%s:%s/hello", dslContainer.getContainerIpAddress(), dslContainer.getMappedPort(exposedPort));
+        String address = String.format("http://%s:%s/hello", dslContainer.getContainerIpAddress(), dslContainer.getFirstMappedPort());
 
-        String address = "http://192.168.1.102:8080/hello";
+
         
         CloseableHttpClient httpClient = HttpClientBuilder.create().build();
         HttpGet get = new HttpGet(address);
